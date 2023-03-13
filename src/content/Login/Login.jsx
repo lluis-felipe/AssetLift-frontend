5f92d2394fa4de18ab99cbc889b3583d7ee57e30
Luis Felipe Ramos da Silva <luisrs0412@ibm.com>
2023-03-13 15:59:04 -0300

    feature-login

---
+++ src/content/Login/Login.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Form,
+  TextInput,
+  InlineLoading,
+  FormGroup,
+  Stack,
+  Theme,
+  PasswordInput,
+} from "@carbon/react";
+import axios from "axios";
+
+function login(username, password, setLoading, setLoginError) {
+  setLoading(true);
+  axios
+    .get("/assetlift/user", {
+      params: {
+        username: username,
+        password: password,
+      },
+    })
+    .then((response) => {
+      // Handle successful response
+      console.log(response.data); // Exibe todos os usuários na resposta do servidor
+      const users = response.data;
+      // Verifica se os nomes de usuário e senhas correspondem
+      const foundUser = users.find((user) => user.username === username && user.password === password);
+      if (foundUser) {
+        console.log("Logged");
+      } else {
+        // Exibe uma mensagem de erro se as credenciais são inválidas
+        setLoading(false);
+        setLoginError(true);
+      }
+    })
+    .catch((error) => {
+      setLoading(false);
+      console.log(error);
+      setLoginError(true);
+    });
+}
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+  const [loginError, setLoginError] = useState(false);
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitting(true);
+    setLoginError(false);
+    login(username, password, setSubmitting, setLoginError);
+  };
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <Theme theme="g10">
+      <Form onSubmit={handleSubmit}>
+        <FormGroup style={{ maxWidth: "400px" }}>
+          <Stack gap={7}>
+            <TextInput
+              id="username"
+              labelText="Username"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+            <PasswordInput
+              id="password"
+              labelText="Password"
+              value={password}
+              onChange={handlePasswordChange}
+              hidePassword={!showPassword}
+              onClickShowPassword={handleTogglePassword}
+              onClickHidePassword={handleTogglePassword}
+            />
+            <Button type="submit" disabled={submitting}>
+              {submitting ? <InlineLoading description="Loading" /> : "Log in"}
+            </Button>
+            {loginError && (
+              <p style={{ color: "red", marginTop: "1rem" }}>
+                Incorrect username or password
+              </p>
+            )}
+          </Stack>
+        </FormGroup>
+      </Form>
+    </Theme>
+  );
+}
+
+export default Login;
